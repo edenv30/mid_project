9fbce7852a8c30fd1df9de6fc211ae3669962e30
eden varsulker <edenv30@gmail.com>
2020-07-08 16:30:09 +0300

    Updates

---
+++ precache-manifest.84363109a9ad49f4d57f6dda8dc3fbf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5837aa87ec4103abaf3a1684afff39a8",
+    "url": "/mid_project/index.html"
+  },
+  {
+    "revision": "36be3adc5f8f5e629f7a",
+    "url": "/mid_project/static/css/main.d3038928.chunk.css"
+  },
+  {
+    "revision": "dfb462c8ee2e65ad5e7f",
+    "url": "/mid_project/static/js/2.8a081686.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mid_project/static/js/2.8a081686.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36be3adc5f8f5e629f7a",
+    "url": "/mid_project/static/js/main.df580ee1.chunk.js"
+  },
+  {
+    "revision": "08911663c5d620acd302",
+    "url": "/mid_project/static/js/runtime-main.8a5452ef.js"
+  }
+]);
